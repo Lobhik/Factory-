06003054d2274faa55ee06a3ce70aeeb4e95efd7
Lobhik juware <61817538+Lobhik@users.noreply.github.com>
2022-06-05 09:27:25 +0530

    Create tree_network.py

---
+++ tree_network.py
@@ -0,0 +1,50 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+
+        return level
+
+    def print_tree(self):
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                #print(child.data)
+                child.print_tree()
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+def build_product_tree():
+    root = TreeNode("TATA STEEL")
+
+    laptop = TreeNode("Factory A")
+    laptop.add_child(TreeNode("Godown A"))
+    laptop.add_child(TreeNode("Godown B"))
+
+    cellphone = TreeNode("Factory B")
+    
+
+    tv = TreeNode("Factory C")
+    tv.add_child(TreeNode("Godown A"))
+    tv.add_child(TreeNode("Godown B"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+
+    root.print_tree()
+
+if __name__ == '__main__':
+    build_product_tree()
